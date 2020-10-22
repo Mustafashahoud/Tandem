36662640a75d880799e4b2d8ce1bf3cabd59b2e8
mustafashahoud <mustafa.shahoud@gmail.com>
2020-10-22 10:42:10 +0200

    Refactored.

--- app/src/test/java/com/mustafa/tandem/api/ApiUtil.kt
+++ app/src/test/java/com/mustafa/tandem/api/ApiUtil.kt
@@ -1,20 +1,12 @@
 package com.mustafa.tandem.api
 
-import com.nhaarman.mockitokotlin2.mock
-import okhttp3.MediaType
-import okhttp3.Request
-import okhttp3.ResponseBody
-import okio.BufferedSource
-import okio.Timeout
-import retrofit2.Call
-import retrofit2.Callback
 import retrofit2.Response
 
 object ApiUtil {
 
-  fun <T : Any> successCall(data: T) = createCall(Response.success(data))
+    fun <T : Any> successCall(data: T) = createCall(Response.success(data))
 
-  fun <T : Any> createCall(response: Response<T>): ApiResponse<T> =
-    ApiResponse.create(200..229, response)
+    fun <T : Any> createCall(response: Response<T>): ApiResponse<T> =
+        ApiResponse.create(200..229, response)
 
 }
