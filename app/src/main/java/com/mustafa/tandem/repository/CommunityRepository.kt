5a6fb353aa96116a4bade79b540e16bec0de93cc
MustafaShahoud <mustafa.shahoud@gmail.com>
2020-11-16 04:37:22 +0100

    Implemented caching functionality using Paging 3 with Flow

--- app/src/main/java/com/mustafa/tandem/repository/CommunityRepository.kt
+++ app/src/main/java/com/mustafa/tandem/repository/CommunityRepository.kt
@@ -1,13 +1,12 @@
 package com.mustafa.tandem.repository
 
-import androidx.lifecycle.LiveData
 import androidx.paging.Pager
 import androidx.paging.PagingConfig
 import androidx.paging.PagingData
-import androidx.paging.liveData
 import com.mustafa.tandem.api.TandemService
 import com.mustafa.tandem.model.Member
 import com.mustafa.tandem.room.AppDatabase
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -18,21 +17,24 @@ class CommunityRepository @Inject constructor(
     private val database: AppDatabase
 ) {
 
-    fun getMembersStream(): LiveData<PagingData<Member>> {
+    fun getMembersStream(): Flow<PagingData<Member>> {
         val pagingSourceFactory = { database.memberDao().queryMembers() }
 
         return Pager(
             PagingConfig(
                 pageSize = NETWORK_PAGE_SIZE,
-                enablePlaceholders = false
+                enablePlaceholders = false,
+                maxSize = 40,
+                prefetchDistance = 5,
+                initialLoadSize = 40
             ),
             remoteMediator = MembersRemoteMediator(service, database),
             pagingSourceFactory = pagingSourceFactory
-        ).liveData
+        ).flow
     }
 
     companion object {
         // Depending on the backend .. in my case it is 20
-        private const val NETWORK_PAGE_SIZE = 10
+        private const val NETWORK_PAGE_SIZE = 20
     }
 }
--- app/src/main/java/com/mustafa/tandem/view/CommunityFragment.kt
+++ app/src/main/java/com/mustafa/tandem/view/CommunityFragment.kt
@@ -7,12 +7,15 @@ import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
 import androidx.paging.LoadState
 import com.mustafa.tandem.R
 import com.mustafa.tandem.databinding.CommunityFragmentBinding
 import com.mustafa.tandem.di.Injectable
 import com.mustafa.tandem.view.adapter.LoadStateAdapter
 import com.mustafa.tandem.view.adapter.MembersAdapter
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class CommunityFragment : Fragment(R.layout.community_fragment), Injectable {
@@ -38,8 +41,10 @@ class CommunityFragment : Fragment(R.layout.community_fragment), Injectable {
     }
 
     private fun subscribeUI() {
-        viewModel.membersStream.observe(viewLifecycleOwner) {
-            pagingAdapter.submitData(viewLifecycleOwner.lifecycle, it)
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewModel.membersStream.collectLatest {
+                pagingAdapter.submitData(it)
+            }
         }
     }
 
@@ -55,18 +60,10 @@ class CommunityFragment : Fragment(R.layout.community_fragment), Injectable {
             footer = LoadStateAdapter { pagingAdapter.retry() }
         )
 
-        // This callback notifies us every time there's a change in the load state via a CombinedLoadStates object.
-        // CombinedLoadStates gives us the load state for the PageSource
-        // Or it gives us the load state for RemoteMediator needed for network and database case
         pagingAdapter.addLoadStateListener { loadState ->
-            // Only show the list if refresh succeeds.
-            binding.membersRecyclerView.isVisible =
-                loadState.source.refresh is LoadState.NotLoading // Not Loading and Not Error -> Success
-            // Show loading spinner during initial load or refresh.
-            binding.progressBar.isVisible = loadState.source.refresh is LoadState.Loading
-            // Show the retry state if initial load or refresh fails.
-            binding.retry.isVisible = loadState.source.refresh is LoadState.Error
-
+            binding.membersRecyclerView.isVisible = loadState.refresh is LoadState.NotLoading
+            binding.progressBar.isVisible = loadState.refresh is LoadState.Loading
+            binding.retry.isVisible = loadState.refresh is LoadState.Error
             // Toast on any error, regardless of whether it came from RemoteMediator or PagingSource
             val errorState = loadState.source.append as? LoadState.Error
                 ?: loadState.source.prepend as? LoadState.Error
@@ -80,6 +77,31 @@ class CommunityFragment : Fragment(R.layout.community_fragment), Injectable {
                 ).show()
             }
         }
+
+        // This callback notifies us every time there's a change in the load state via a CombinedLoadStates object.
+        // CombinedLoadStates gives us the load state for the PageSource
+        // Or it gives us the load state for RemoteMediator needed for network and database case
+//        pagingAdapter.addLoadStateListener { loadState ->
+//            // Only show the list if refresh succeeds.
+//            binding.membersRecyclerView.isVisible = loadState.source.refresh is LoadState.NotLoading // Not Loading and Not Error -> Success
+//            // Show loading spinner during initial load or refresh.
+//            binding.progressBar.isVisible = loadState.source.refresh is LoadState.Loading
+//            // Show the retry state if initial load or refresh fails.
+//            binding.retry.isVisible = loadState.source.refresh is LoadState.Error
+//
+//            // Toast on any error, regardless of whether it came from RemoteMediator or PagingSource
+//            val errorState = loadState.source.append as? LoadState.Error
+//                ?: loadState.source.prepend as? LoadState.Error
+//                ?: loadState.append as? LoadState.Error
+//                ?: loadState.prepend as? LoadState.Error
+//            errorState?.let {
+//                Toast.makeText(
+//                    requireContext(),
+//                    "\uD83D\uDE28 Wooops ${it.error}",
+//                    Toast.LENGTH_LONG
+//                ).show()
+//            }
+//        }
     }
 
     override fun onDestroyView() {
